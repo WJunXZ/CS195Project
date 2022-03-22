62dd9c29f3df60a47455a516293b494c63ce0c3d
Lenni <lennart@familie-richter.de>
2022-03-21 22:17:58 -0700

    Added Post Page

--- Forum/post/views.py
+++ Forum/post/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def postPage(request):
+    id = request.GET.get('post_id')
+    return render(request, "base.html", {"post_id": id})
