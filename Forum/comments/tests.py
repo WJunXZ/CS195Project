e78b371fbc5f9142f005d4b0b2bd9f4c27174bf3
Wiseley Zhang <Admin@wiseleys-mbp.gateway.sonic.net>
2022-05-16 23:10:35 -0700

    added test cases

--- Forum/comments/tests.py
+++ Forum/comments/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
-
+from django.db import IntegrityError
+from .models import Comment
+from posts.models import Post
+from django.contrib.auth.models import User
+import unittest
+from django.test import Client
 # Create your tests here.
+
+
+class CommentIntegrationTest(TestCase):
+
+    def test_post(self):
+   	    User.objects.create(username='TestUser')
+   	    #print(User.objects.all())
+   	    test_user = User.objects.get(username = 'TestUser')
+   	    Post.objects.create(user= test_user, title = "test", content="this is a test") 
+   	    #print(Post.objects.all())
+   	    test_post = Post.objects.get(title="test")
+   	    self.assertEqual(test_post.content, "this is a test" )
+   	    Comment.objects.create(user = test_user, original_post= test_post, title="test comment title", content="test comment")
+   	    test_comment = Comment.objects.get(title="test comment title")
+   	    self.assertEqual(test_comment.content, "test comment")
+    #
+
+class CommentTestCaseNoUserOrPost(TestCase):
+
+    def test_post(self):
+    	try:
+    		Comment.objects.create(user = None, original_post= None, title="test comment title", content="test comment") 
+    	except IntegrityError as e:
+    		self.assertEqual(1, 1)
+
+    #
+
+
+
--- Forum/posts/tests.py
+++ Forum/posts/tests.py
@@ -1,3 +1,46 @@
 from django.test import TestCase
-
+from django.db import IntegrityError
+from .models import Post
+from django.contrib.auth.models import User
+import unittest
+from django.test import Client
 # Create your tests here.
+
+
+class PostIntegrationTest(TestCase):
+    def setup(self):
+        User.objects.create(username='Testuser')
+        test_user = User.objects.get(username = 'Testuser')
+        Post.objects.create(user= test_user, title = "test", content="this is a test") 
+
+    def test_post(self):
+   	    User.objects.create(username='TestUser')
+   	    #print(User.objects.all())
+   	    test_user = User.objects.get(username = 'TestUser')
+   	    Post.objects.create(user= test_user, title = "test", content="this is a test") 
+   	    #print(Post.objects.all())
+   	    test_post = Post.objects.get(title="test")
+   	    self.assertEqual(test_post.content, "this is a test" )
+    #
+
+
+class MissingUserIntegrityTest(TestCase):
+
+    def test_post(self):
+    	try:
+    		Post.objects.create(user = None, title="test post title", content="test post") 
+    	except IntegrityError as e:
+    		self.assertEqual(1, 1)
+
+    #
+
+
+
+
+
+
+
+
+
+
+
